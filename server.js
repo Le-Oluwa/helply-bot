5d9e0993483fd8bea2883a83d7a7c4eabaa15e4a
Le-Oluwa <114214155+Le-Oluwa@users.noreply.github.com>
2026-04-25 13:56:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -81,7 +81,8 @@ app.post("/webhook", (req, res) => {
 // ================= START SERVER =================
 const PORT = process.env.PORT || 3000;
 
-// 🔥 IMPORTANT: bind to 0.0.0.0
+console.log("FINAL PORT:", PORT);
+
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
