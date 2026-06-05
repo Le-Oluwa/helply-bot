ae826cd5c36399ea3eb510d569139893d99a2782
Le-Oluwa <114214155+Le-Oluwa@users.noreply.github.com>
2026-06-05 23:42:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -118,10 +118,6 @@ By continuing, you agree to these Terms & Conditions.`,
 Send your request`);
 });
 
-bot.on("message", (msg) => {
-  console.log("CHAT ID:", msg.chat.id);
-});
-
 // ================= MESSAGE =================
 bot.on("message", async (msg) => {
   if (!msg.text || msg.text.startsWith("/")) return;
